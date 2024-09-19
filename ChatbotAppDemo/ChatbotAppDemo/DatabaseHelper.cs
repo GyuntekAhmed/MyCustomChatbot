b512676fda273c955be3c3984fccc32b1725f40a
Gyuntek Ahmed <gyuntek.ahmed@sistechnology.com>
2024-09-19 15:59:07 +0300

    Init Repo

---
+++ ChatbotAppDemo/ChatbotAppDemo/DatabaseHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+
+public class DatabaseHelper
+{
+	private string connectionString = "Server=.;Database=SisTechnologyDB;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true;";
+
+	public List<(string Question, string Answer)> GetFAQs()
+	{
+		var faqs = new List<(string Question, string Answer)>();
+
+		using (SqlConnection connection = new SqlConnection(connectionString))
+		{
+			connection.Open();
+			string query = "SELECT Question, Answer FROM FAQ";
+			SqlCommand command = new SqlCommand(query, connection);
+			SqlDataReader reader = command.ExecuteReader();
+
+			while (reader.Read())
+			{
+				faqs.Add((reader["Question"].ToString(), reader["Answer"].ToString()));
+			}
+		}
+
+		return faqs;
+	}
+}
---
+++ ChatbotAppDemo/ChatbotAppDemo/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace ChatbotAppDemo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void sendBtn_Click(object sender, RoutedEventArgs e)
+		{
+			string userMessage = UserMessageTextBox.Text;
+
+			var faqs = new DatabaseHelper().GetFAQs();
+
+			var (Question, Answer) = faqs.FirstOrDefault(f => f.Question == userMessage);
+			var question = Question;
+			var answer = Answer;
+
+			BotResponseTextBox.Text = answer;
+		}
+	}
+}
